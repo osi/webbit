1001c6c1301e65a0df5f640a4313e4467138e040
peter royal <proyal@drw.com>
2014-01-31 13:56:36 -0600

    fix test. null vs empty string is now returned

--- src/test/java/org/webbitserver/helpers/QueryParametersTest.java
+++ src/test/java/org/webbitserver/helpers/QueryParametersTest.java
@@ -8,21 +8,22 @@ import static org.junit.Assert.assertEquals;
 public class QueryParametersTest {
     @Test
     public void parsesOneParameter() throws Exception {
-        assertEquals("bar", new QueryParameters("foo=bar", true).first("foo"));
+        assertEquals("bar", new QueryParameters("foo=bar", false).first("foo"));
     }
 
     @Test
     public void parsesTwoParameters() throws Exception {
-        assertEquals(asList("b", "e"), new QueryParameters("a=b&c=d&a=e", true).all("a"));
+        assertEquals(asList("b", "e"), new QueryParameters("a=b&c=d&a=e", false).all("a"));
     }
 
     @Test
     public void parsesEmptyParameter() throws Exception {
-        assertEquals(null, new QueryParameters("a=", true).first("a"));
+        assertEquals("", new QueryParameters("a=", false).first("a"));
     }
 
     @Test
     public void parsesMixOfPresentAndEmptyParameters() throws Exception {
-        assertEquals(asList("b", null, "e"), new QueryParameters("a=b&a=&a=e", true).all("a"));
+        assertEquals(asList("b", "", "e"), new QueryParameters("a=b&a=&a=e", false).all("a"));
+
     }
 }
